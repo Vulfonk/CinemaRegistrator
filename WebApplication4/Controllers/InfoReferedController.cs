d645c1cca49f7446ee6127647cd0d49b2ffd8d24
Vulfonk <podkalyuk-dima@bk.ru>
2024-05-30 03:02:46 +0500

    feat: добавил ручку досттупные места для сеанса

---
+++ WebApplication4/Controllers/InfoReferedController.cs
@@ -0,0 +1,18 @@
+using CinemaDB.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Data;
+
+namespace WebApplication4.Controllers
+{
+    [ApiController]    
+    [Route("infoReserved")]
+    public class InfoReferedController : ControllerBase
+    {
+        [HttpGet(Name = "infoReserved")]
+        public AvailibleSeats Get(int sessionId)
+        {
+            var helper = new DbHelper();            
+            return helper.GetSessionById(1);
+        }
+    }
+}
